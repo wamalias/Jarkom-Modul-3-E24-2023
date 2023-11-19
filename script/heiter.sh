51015c0860d80499c9b967da0e5b272d3ff4005b
wamalias <111508713+wamalias@users.noreply.github.com>
2023-11-19 21:28:16 +0700

    Update heiter.sh

--- script/heiter.sh
+++ script/heiter.sh
@@ -24,3 +24,40 @@ $TTL    604800
 @       IN      SOA     riegel.canyon.e24.com root.riegel.canyon.e24.com. (
                                     2023110101    ; Serial
                         604800        ; Refresh
+                        86400         ; Retry
+                        2419200       ; Expire
+                        604800 )      ; Negative Cache TTL
+;
+@               IN      NS      riegel.canyon.E14.com.
+@               IN      A       192.218.4.1         ; IP Fern
+
+' >  /etc/bind/jarkom/riegel.canyon.e24.com
+
+echo '
+;
+; BIND data file for local loopback interface
+;
+$TTL    604800
+@       IN      SOA     granz.channel.e24.com root.granz.channel.e24.com. (
+                                    2023110101    ; Serial
+                        604800        ; Refresh
+                        86400         ; Retry
+                        2419200       ; Expire
+                        604800 )      ; Negative Cache TTL
+;
+@               IN      NS      granz.channel.E14.com.
+@               IN      A       192.218.3.1         ; IP Lugner
+' >  /etc/bind/jarkom/granz.channel.e24.com
+
+echo 'options {
+        directory "/var/cache/bind";
+
+        forwarders {
+                   192.168.122.1;
+          };
+        //dnssec-validation auto;
+        allow-query{ any; };
+        listen-on-v6 { any; };
+};' > /etc/bind/named.conf.options
+
+service bind9 restart
